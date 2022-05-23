65be8a549fa0900bb4925f9bbf23af8c20e6d364
Vladimir Belebnev <burittoo2017@icloud.com>
2022-05-23 19:11:46 +0300

    refactoring code

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -6,63 +6,58 @@ import './App.scss';
 
 const App = () => {
 	const [todos, setTodos] = useState(localStorage.getItem('todos') ? JSON.parse(localStorage.getItem('todos')) : []);
+
+	const setTodosFunc = (arr) => {
+		setTodos(arr);
+		localStorage.setItem('todos', JSON.stringify(arr));
+	};
 	
-	const addTask = (userInput) => {
-		if (userInput) {
-			const newItem = {
+	const addTask = (userTask) => {
+		if (userTask) {
+			const newTask = {
 				id: Math.random().toString(36).substr(2, 9),
-				task: userInput,
+				task: userTask,
 				complete: false,
 				star: false,
-				index: 1,
 			};
-			setTodos([...todos, newItem]);
-			localStorage.setItem('todos', JSON.stringify([...todos, newItem]))
+			setTodosFunc([...todos, newTask]);
 		}
 	};
 
 	const removeTask = (id) => {
-		setTodos([...todos.filter((task) => task.id !== id)]);
-		localStorage.setItem('todos', JSON.stringify([...todos.filter((task) => task.id !== id)]))
+		setTodosFunc([...todos.filter((task) => task.id !== id)]);
 	};
 
 	const handleToggleStar = (id) => {
-		setTodos([...todos.map((task) =>
+		setTodosFunc([...todos.map((task) => 
 			task.id === id ? { ...task, star: !task.star } : { ...task }
 		)]);
-		localStorage.setItem('todos', JSON.stringify([...todos.map((task) =>
-			task.id === id ? { ...task, star: !task.star } : { ...task }
-		)]));
 	};
 
-	const handleToggle = (id) => {
-		setTodos([...todos.map((task) => 
-			task.id === id ? {...task, complete: !task.complete} : {...task}
-		)])
-		localStorage.setItem('todos', JSON.stringify([...todos.map((task) =>
+	const handleToggleComplete = (id) => {
+		setTodosFunc([...todos.map((task) =>
 			task.id === id ? { ...task, complete: !task.complete } : { ...task }
-		)]));
+		)]);
 	};
 
   	return (
     	<div className='App'>
 			<h2 className="App-header">React toDo</h2>
-				<p className="App-desc"><b>React toDo</b> – список дел и таск–менеджер №1 в мире. Мощный инструмент. Стоит только попробовать.</p>
+				<p className="App-desc"><b>React toDo</b> – список дел и таск–менеджер №1 в мире. Мощный инструмент. Стоит только попробовать!</p>
 			<AppForm 
 				addTask={addTask}
 				todos={todos} />
-			{todos.map((task, i) => {
-				return (
-					<AppItem 
-						task={task}
-						key={task.id}
-						toggleTask={handleToggle}
-						toggleStar={handleToggleStar}
-						removeTask={removeTask}
-						index={++i}
-					/>
-				)
-			})}
+				{todos.map((task, i) => {
+					return (
+						<AppItem 
+							task={task}
+							key={task.id}
+							toggleTask={handleToggleComplete}
+							toggleStar={handleToggleStar}
+							removeTask={removeTask}
+							index={++i} />
+					)
+				})}
 		</div>
 	);
 };		
